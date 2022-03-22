d7bb07475ac3f08c2fecfa8c363987cdbe727427
Bradd Evans <braddevans123@gmail.com>
2022-03-22 17:49:36 +0000

    feature(controllers & models): Amend to messages endpoint for database and api
    new files:
    - messages.controller.js
    - messages.model.js
    
    BREAKING CHANGE: messages
    
    Closes 1

--- packages/express-api/models/messages.model.js
+++ packages/express-api/models/messages.model.js
@@ -1,14 +1,20 @@
-const { Model, DataTypes } = require('sequelize');
-module.exports = (sequelize, Sequelize) => {
-  class Messages extends Model {}
-  Messages.init({
-    message_id: DataTypes.INTEGER,
-    user_id: DataTypes.INTEGER,
-    username: DataTypes.STRING,
-    Message: DataTypes.STRING(6000),
-  }, { sequelize, modelName: 'messages' });
+const {Model, DataTypes} = require('sequelize');
+module.exports = (sequelize) => {
+    class Messages extends Model {
+    }
 
-  Messages.sync();
+    Messages.init({
+        message_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: DataTypes.INTEGER,
+        username: DataTypes.STRING,
+        Message: DataTypes.STRING(6000),
+    }, {sequelize, modelName: 'messages'});
 
-  return Messages;
+    Messages.sync();
+
+    return Messages;
 };
