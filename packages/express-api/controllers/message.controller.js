4c1a35e07317d703228e5e034f9a074433542853
Bradd Evans <braddevans123@gmail.com>
2022-03-22 17:49:36 +0000

    feature(controllers & models): Amend to messages endpoint for database and api
    new files:
    - messages.controller.js
    - messages.model.js
    
    BREAKING CHANGE: messages
    
    Closes 1

--- packages/express-api/controllers/message.controller.js
+++ packages/express-api/controllers/message.controller.js
@@ -1,8 +1,33 @@
 const db = require("../models");
+const {Op} = require("sequelize");
 const User = db.user;
 const Message = db.messages;
 
 
 exports.createMessage = (req, res) => {
-  res.status(200).send("User_id: " + req.userId);
+    if (req.body.message == null) {
+        res.status(404).send("Cannot send empty message");
+    } else {
+        let userID = req.userId;
+        let timestamp = new Date().getTime();
+        let user = User.findAll({
+            where: {
+                id: {
+                    [Op.eq]: userID
+                }
+            }
+        });
+        let message = {
+            body: req.body,
+            messageID: Message.create({
+                user_id: userID,
+                username: user.username,
+                message: req.body,
+                created_at: timestamp,
+                updated_at: timestamp
+            })
+        }
+
+        res.status(200).send(JSON.stringify(message));
+    }
 };
